7b5d04644108d93843f28759fadc4beb60d89709
ciprian.anghel <anghelciprian91@gmail.com>
2020-03-06 22:13:08 +0200

    Continuing with c6

---
+++ src/cap7/multithreading/executorservice/SingleThreadExecutorMain.java
@@ -0,0 +1,50 @@
+package cap7.multithreading.executorservice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadExecutorMain {
+
+    public static void main(String[] args) {
+
+        ExecutorService service = null;
+
+        try {
+            service = Executors.newSingleThreadExecutor();
+
+            System.out.println("Begin!");
+
+            service.execute(() -> System.out.println("Printing inventory"));
+            service.execute(() -> { for (int i = 1; i < 10; i++) {
+                System.out.println("Printing thead 1 " + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+                });
+
+            service.shutdown(); //Throws RejectExecutionException and does not accept any new task
+//            service.shutdownNow();
+
+            service.execute(() -> { for (int i = 1; i < 10; i++) {
+                System.out.println("Printing thread 2" + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            });
+            service.execute(() -> System.out.println("Printing inventory"));
+
+            System.out.println("End!");
+
+        } finally {
+            service.shutdown();
+        }
+
+    }
+
+}
