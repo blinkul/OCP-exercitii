9fb81446ddbe76d6410ffd816a118a4032be4bbd
ciprian.anghel <anghelciprian91@gmail.com>
2020-03-06 22:44:56 +0200

    Continuing with c6

---
+++ src/cap7/multithreading/executorservice/AwaitTerminationMain.java
@@ -0,0 +1,57 @@
+package cap7.multithreading.executorservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+//THIS IS MISSING SOMETHING
+public class AwaitTerminationMain {
+
+    public static void main(String[] args) {
+
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+            service.execute(new TaskCreator(1));
+            service.execute(new TaskCreator(2));
+        } finally {
+            System.out.println("Service will be shutting down!");
+            service.shutdown();
+        }
+
+        try {
+            service.awaitTermination(15, TimeUnit.SECONDS);
+            if (service.isTerminated()) {
+                System.out.println("All processes are completed!");
+            } else {
+                System.out.println("At least one process is running!");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Awaiting termination has been cancelled");
+        }
+
+    }
+}
+
+class TaskCreator implements Runnable {
+
+    private int number;
+
+    public TaskCreator(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 10; i > 1; i--) {
+            try {
+                System.out.println("(" + number + ") Take off in " + i + "s");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("(" + number + ") Launch has been interrupted!");
+            }
+        }
+        System.out.println("(" + number + ") ROCKET IS TAKING OFF!");
+    }
+
+}
